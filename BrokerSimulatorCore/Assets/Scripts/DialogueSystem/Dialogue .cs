e80c61884bae9485ef370a87341e75dbe2ad07f2
Michał <michal_szkopinski@op.pl>
2018-04-23 17:25:18 +0200

    Base dialogue system

---
+++ BrokerSimulatorCore/Assets/Scripts/DialogueSystem/Dialogue .cs
@@ -0,0 +1,7 @@
+﻿namespace Assets.Scripts.DialogueSystem
+{
+    public class Dialogue 
+    {
+        
+    }
+}
---
+++ BrokerSimulatorCore/Assets/Scripts/DialogueSystem/EventArgs/DialogueContinuedEventArgs.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.Scripts.DialogueSystem.EventArgs
+{
+    public class DialogueContinuedEventArgs : System.EventArgs
+    {
+        public DialogueContinuedEventArgs()
+        {
+            
+        }
+    }
+}
